2221fd1a1128ef5ac3f9444e5ae1c92d5f011834
Xiaodong Li <bryne_lxd@sina.com>
2020-03-29 21:25:11 +0800

    updated jenkins_job.sh

--- jenkins_job.sh
+++ jenkins_job.sh
@@ -8,7 +8,7 @@ rm -rf "${WORKSPACE}/fsdemo-usercenter" || true
 # get the change_image.sh and yaml file
 git clone https://github.com/brynelee/fsdemo-usercenter.git
 cp ./fsdemo-usercenter/fsdemo-common-tools/docker/change_image.sh .
-cp "./fsdemo-usercenter/fsdemo-common-tools/kubernetes/"$1".yml" .
+cp ./fsdemo-usercenter/fsdemo-common-tools/kubernetes/$1.yml .
 rm -rf ./fsdemo-usercenter
 #wget --no-cookie --no-check-certificate https://raw.githubusercontent.com/brynelee/fsdemo-usercenter/master/fsdemo-common-tools/docker/change_image.sh
 #wget --no-cookie --no-check-certificate https://raw.githubusercontent.com/brynelee/fsdemo-usercenter/master/fsdemo-common-tools/kubernetes/$1.yml
